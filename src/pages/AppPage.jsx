c0be6f90e7913160559481fa4094a3f890ab5fd2
Youichi Uehara <youichiuehara14@gmail.com>
2025-04-04 16:15:33 +0800

    fix typo error

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar';
 import Footer from '../components/Footer';
-import FindADrink from '../components/findadrink';
-import RandomCards from '../components/RandomCards';
+import FindADrink from '../components/FindADrink';
 
 const AppPage = () => {
   return (
@@ -14,7 +13,6 @@ const AppPage = () => {
         <div>
           <FindADrink />
         </div>
-
         <div className="mt-auto py-3  border-t-1   border-[#5c5c5c] shadow-2xl shadow-[#ffcab5]  ">
           <div className="max-w-10/11 mx-auto ">
             <Footer />
