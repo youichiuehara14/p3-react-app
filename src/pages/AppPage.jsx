85ed4362d6cd56ecdd1f955c9e9fa8f09438b3e8
Youichi Uehara <youichiuehara14@gmail.com>
2025-04-17 14:58:35 +0800

    attempt fix(issue identified, FindADrink component causes the issue)

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -1,13 +1,13 @@
 import NavigationBar from '../components/NavigationBar';
 import Footer from '../components/Footer';
-import FindADrink from '../components/FindADrink';
+// import FindADrink from '../components/FindADrink';
 
 const AppPage = () => {
   return (
     <>
       <div className="h-screen flex flex-col">
         <NavigationBar />
-        <FindADrink />
+        {/* <FindADrink /> */}
         <div className="mt-auto py-3  border-t-1 border-[#5c5c5c] shadow-2xl shadow-[#ffcab5]  ">
           <div className="max-w-10/11 mx-auto ">
             <Footer />
